5ba0c2c037e266a7ce21842c5bae05d63af2c9e5
mihabor2002 <misha.borovikov@gmail.com>
2021-06-04 11:16:17 +0300

    add 2 files

---
+++ Project2/Interpolation.plt
@@ -0,0 +1,2 @@
+set pm3d
+splot[-1.5:1.5][-1.5:1.5][-1.2:1.2] sin(x**2 + y**2), 'Interpolation.txt' 
---
+++ Project2/Segment.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <ctime>
+using namespace std;
+#define CLOUD 20
+#define E 0.01
+#define pi 3.1415926535
+#include "point.h"
+class Segment
+{
+private:
+    Point A;
+    Point B;
+    int S; // ninoiyiea ?aaii 1- aino?iai 0-iaaino?iai
+public:
+    Segment(const Segment& p) = default;
+    Segment() = default;
+    ~Segment() = default;
+
+    void Set_Tip(const int& T) { S = T; }
+    void Set_A(const Point& PP) { A = PP; }
+    void Set_B(const Point& PP) { B = PP; }
+
+    int Get_S() { return S; }
+    Point Get_A() { return A; }
+    Point Get_B() { return B; }
+};
